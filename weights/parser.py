6d351488d6f6ce1d2ec7f4c830f8974d2c54d8e0
parvvaresh <parvvaresh@gmail.com>
2025-02-11 07:46:53 +0330

    fix bugs :: str path

--- weights/parser.py
+++ weights/parser.py
@@ -4,9 +4,9 @@ import sys
 
 file_path = Path(__file__).resolve()
 
-root_path = file_path.parent
+root_path = str(file_path.parent)
 
 def get_path_model_object():
     return root_path + "/best.pt"
 def get_path_model_char():
-    return root_path + "/yoyolov8n_char_new.pt"
+    return root_path + "/yolov8n_char_new.pt"
