86e6404ffa69df3ce951af5fba7e0c472c1af285
parvvaresh <parvvaresh@gmail.com>
2025-02-25 12:58:41 +0330

    add new model

--- weights/parser.py
+++ weights/parser.py
@@ -7,6 +7,6 @@ file_path = Path(__file__).resolve()
 root_path = str(file_path.parent)
 
 def get_path_model_object():
-    return root_path + "/best.pt"
+    return root_path + "/plateYolo.pt"
 def get_path_model_char():
-    return root_path + "/yolov8n_char_new.pt"
+    return root_path + "/CharsYolo.pt"
